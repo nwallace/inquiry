1a866f1e293e1d12965e90eb1f0e90f99bbd677f
Nathan Wallace <nathan.m.wallace@gmail.com>
2016-04-04 12:52:18 -0400

    fix bug where search would raise error if no sort orders or no search clauses configured

--- lib/inquiry/search.rb
+++ lib/inquiry/search.rb
@@ -2,23 +2,27 @@ module Inquiry
   module Search
     module ClassMethods
       def search_clause(search_key, filter_clause, options={})
-        @search_clauses ||= []
-        @search_clauses << SearchClause.new(search_key, filter_clause, options)
+        search_clauses << SearchClause.new(search_key, filter_clause, options)
       end
 
       def sort_order(sort_key, sort_clause, options={})
-        @sort_orders ||= []
-        @sort_orders << SortClause.new(sort_key, sort_clause, options)
+        sort_orders << SortClause.new(sort_key, sort_clause, options)
       end
 
       def search(search_parameters={})
-        (@search_clauses + @sort_orders).inject(model_class.all) do |scope, search_clause|
-          search_clause.apply(scope, search_parameters)
+        (search_clauses + sort_orders).inject(base_scope) do |scope, query_clause|
+          query_clause.apply(scope, search_parameters)
         end
       end
 
-      def model_class
-        @model_class ||= self.name.to_s.sub(/Search$/, "").constantize
+      private
+
+      def search_clauses
+        @search_clauses ||= []
+      end
+
+      def sort_orders
+        @sort_orders ||= []
       end
     end
 
